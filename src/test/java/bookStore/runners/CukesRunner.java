5506763cc0be6d6869c71f2457a5ce7e4224c098
SametEmsen <sametemsen@gmail.com>
2024-08-08 11:35:32 +0300

    Jenkinsfile added

--- src/test/java/bookStore/runners/CukesRunner.java
+++ src/test/java/bookStore/runners/CukesRunner.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports.html",
-                "rerun:target/rerun.txt",
                 "pretty"},
         features = "src/test/resources/features",
         glue ="bookStore/step_defs",
